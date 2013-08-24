5825f3d5ec1bd0b3edc90ba99731b982f3b9613e
Omar MEBARKI <omar.mebarki@gmail.com>
2013-08-24 23:44:56 +0200

    add maven-dependencies

--- java-hadoop-poc/src/main/java/omar/mebarki/hadoop/poc/mapreduce/WordCount.java
+++ java-hadoop-poc/src/main/java/omar/mebarki/hadoop/poc/mapreduce/WordCount.java
@@ -1,11 +1,33 @@
 package omar.mebarki.hadoop.poc.mapreduce;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 /**
- * Created with IntelliJ IDEA.
- * User: retina
+ * User: Omar MEBARKI
  * Date: 24/08/13
  * Time: 23:12
- * To change this template use File | Settings | File Templates.
  */
 public class WordCount {
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                output.collect(word, one);
+            }
+        }
+    }
 }
