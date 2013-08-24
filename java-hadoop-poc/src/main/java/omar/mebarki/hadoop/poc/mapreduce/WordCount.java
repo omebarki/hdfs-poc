dfbf4f7240e0d6ec5bd5ec9503f852ea567eeeec
Omar MEBARKI <omar.mebarki@gmail.com>
2013-08-24 23:18:17 +0200

    project setup

---
+++ java-hadoop-poc/src/main/java/omar/mebarki/hadoop/poc/mapreduce/WordCount.java
@@ -0,0 +1,11 @@
+package omar.mebarki.hadoop.poc.mapreduce;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: retina
+ * Date: 24/08/13
+ * Time: 23:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class WordCount {
+}
